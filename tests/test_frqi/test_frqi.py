fc83c7f3d9d466092388c90e2f443ba5442900d1
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-09-16 01:12:57 -0300

    Adding tests for RGB images

--- tests/test_frqi/test_frqi.py
+++ tests/test_frqi/test_frqi.py
@@ -2,6 +2,8 @@ import numpy as np
 from frqi import FRQI
 from qiskit import execute
 from qiskit.providers.aer.backends import AerSimulator
+from skimage import data
+from skimage.transform import resize
 
 
 class TestFRQI:
@@ -13,6 +15,7 @@ class TestFRQI:
     IMAGE1 = np.array([[0, 0], [0, 0]])
     IMAGE2 = np.array([[1, 1], [1, 1]])
     IMAGE3 = np.array([[0.5, 0.5], [0.5, 0.5]])
+    ASTRONAUT = resize(data.astronaut(), (2, 2))
 
     def test_result_image1(self):
 
@@ -62,19 +65,30 @@ class TestFRQI:
     def test_image_qc_gates(self):
 
         qc = self.FRQI.image_quantum_circuit(image=self.IMAGE1, measurements=True)
+        qc_rgb = self.FRQI.image_quantum_circuit(
+            image=self.ASTRONAUT, measurements=True
+        )
 
         circuit_gates = list(qc.count_ops())
         result_test_gates = [gate in self.GATE_SET for gate in circuit_gates]
+        circuit_gates_rgb = list(qc_rgb.count_ops())
+        result_test_gates_rgb = [gate in self.GATE_SET for gate in circuit_gates_rgb]
         gates_true_list = np.ones(len(result_test_gates))
 
         assert np.allclose(result_test_gates, gates_true_list)
+        assert np.allclose(result_test_gates_rgb, gates_true_list)
 
     def test_image_qc_gate_count(self):
 
         qc = self.FRQI.image_quantum_circuit(image=self.IMAGE1, measurements=True)
+        qc_rgb = self.FRQI.image_quantum_circuit(
+            image=self.ASTRONAUT, measurements=True
+        )
 
         qc_gates_dict = dict(qc.count_ops())
         del qc_gates_dict["barrier"]
+        qc_gates_dict_rgb = dict(qc_rgb.count_ops())
+        del qc_gates_dict_rgb["barrier"]
 
         aux_bin_list = [
             bin(i)[2:] for i in range(self.IMAGE1.shape[0] * self.IMAGE1.shape[1])
@@ -106,6 +120,19 @@ class TestFRQI:
             qc_gates_dict["x"],
             qc_gates_dict["ccry"],
         ]
+        qc_rgb_count_gate_list = [
+            qc_gates_dict_rgb["h"],
+            qc_gates_dict_rgb["measure"],
+            qc_gates_dict_rgb["x"],
+            qc_gates_dict_rgb["ccry"],
+        ]
         count_gate_list = [hadamard_count, measure_count, x_count, mcry_count]
+        count_gate_list_rgb = [
+            hadamard_count,
+            measure_count + 2,
+            3 * x_count,
+            3 * mcry_count,
+        ]
 
         assert np.allclose(qc_count_gate_list, count_gate_list)
+        assert np.allclose(qc_rgb_count_gate_list, count_gate_list_rgb)
