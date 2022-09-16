6cfb2699a3724ef023a3d565dccdf13fa93739ed
jvscursulim <josevictor.s.scursulim@gmail.com>
2022-09-16 03:46:10 -0300

    Addings tests for RGB NEQR circuits

--- tests/test_neqr/test_neqr.py
+++ tests/test_neqr/test_neqr.py
@@ -11,6 +11,7 @@ class TestNEQR:
 
     GATE_SET = {"ccx", "mcx", "h", "x", "measure", "barrier"}
     ASTRONAUT_IMAGE_GRAY = rgb2gray(data.astronaut())
+    ASTRONAUT_IMAGE_RGB = data.astronaut()
     ZERO_IMAGE_MATRIX = np.array([[0, 0, 0], [0, 1, 0], [0, 1, 0], [0, 0, 0]])
     NEQR = NEQR()
     SHOTS = 8192
@@ -18,57 +19,96 @@ class TestNEQR:
 
     def _prepare_pixel_intensity_binary_dict(self, image_matrix: np.ndarray) -> dict:
 
-        pixels_intensity = []
-        for row in image_matrix:
-            for entry in row:
-                intensity = int(np.round(255 * entry))
-                pixels_intensity.append(intensity)
+        if len(image_matrix.shape) == 2:
+            n = 1
+        else:
+            n = len(image_matrix.shape)
+            key_value_list = []
 
-        aux_binary_pixel_intensity = [
-            bin(p_intensity)[2:] for p_intensity in pixels_intensity
-        ]
-        aux_len_bin_list = [
-            len(binary_num) for binary_num in aux_binary_pixel_intensity
-        ]
-        max_length = max(aux_len_bin_list)
-        binary_pixel_intensity = []
+        for i in range(n):
 
-        for bnum in aux_binary_pixel_intensity:
-            if len(bnum) < max_length:
-                new_binary = ""
-                for _ in range(max_length - len(bnum)):
-                    new_binary += "0"
-                new_binary += bnum
-                binary_pixel_intensity.append(new_binary)
+            pixels_intensity = []
+            if n == 1:
+                pixels_matrix = image_matrix
             else:
-                binary_pixel_intensity.append(bnum)
+                pixels_matrix = image_matrix[:, :, i]
+            for row in pixels_matrix:
+                for entry in row:
+                    intensity = int(np.round(255 * entry))
+                    pixels_intensity.append(intensity)
 
-        aux_bin_list = [bin(i)[2:] for i in range(len(binary_pixel_intensity))]
-        aux_len_bin_list = [len(binary_num) for binary_num in aux_bin_list]
-        max_length = max(aux_len_bin_list)
-        binary_list = []
+            aux_binary_pixel_intensity = [
+                bin(p_intensity)[2:] for p_intensity in pixels_intensity
+            ]
+            aux_len_bin_list = [
+                len(binary_num) for binary_num in aux_binary_pixel_intensity
+            ]
+            max_length = max(aux_len_bin_list)
+            binary_pixel_intensity = []
 
-        for bnum in aux_bin_list:
-            if len(bnum) < max_length:
-                new_binary = ""
-                for _ in range(max_length - len(bnum)):
-                    new_binary += "0"
-                new_binary += bnum
-                binary_list.append(new_binary)
-            else:
-                binary_list.append(bnum)
+            for bnum in aux_binary_pixel_intensity:
+                if len(bnum) < max_length:
+                    new_binary = ""
+                    for _ in range(max_length - len(bnum)):
+                        new_binary += "0"
+                    new_binary += bnum
+                    binary_pixel_intensity.append(new_binary)
+                else:
+                    binary_pixel_intensity.append(bnum)
 
-        pixel_intensity_binary_dict = {
-            tp[0]: tp[1] for tp in zip(binary_list, binary_pixel_intensity)
-        }
+            aux_bin_list = [bin(i)[2:] for i in range(len(binary_pixel_intensity))]
+            aux_len_bin_list = [len(binary_num) for binary_num in aux_bin_list]
+            max_length = max(aux_len_bin_list)
+            binary_list = []
+
+            for bnum in aux_bin_list:
+                if len(bnum) < max_length:
+                    new_binary = ""
+                    for _ in range(max_length - len(bnum)):
+                        new_binary += "0"
+                    new_binary += bnum
+                    binary_list.append(new_binary)
+                else:
+                    binary_list.append(bnum)
+
+            if n != 1:
+                if i == 0:
+                    new_binary_list = ["00 " + str_bin for str_bin in binary_list]
+                elif i == 1:
+                    new_binary_list = ["01 " + str_bin for str_bin in binary_list]
+                elif i == 2:
+                    new_binary_list = ["10 " + str_bin for str_bin in binary_list]
+                key_value_list.append(
+                    list(zip(new_binary_list, binary_pixel_intensity))
+                )
+
+        if n == 1:
+            pixel_intensity_binary_dict = {
+                tp[0]: tp[1] for tp in zip(binary_list, binary_pixel_intensity)
+            }
+        else:
+            pixel_intensity_binary_dict = {}
+            for item in key_value_list:
+                for tp in item:
+                    pixel_intensity_binary_dict[tp[0]] = tp[1]
 
         return pixel_intensity_binary_dict
 
     def _process_counts(self, counts: dict, image_matrix: np.ndarray) -> dict:
 
-        num_pixels = image_matrix.shape[0] * image_matrix.shape[1]
-        keys_list = [key for key, _ in sorted(counts.items())][:num_pixels]
-        processed_counts = {key.split(" ")[0]: key.split(" ")[1] for key in keys_list}
+        if len(image_matrix.shape) == 3:
+            num_pixels = image_matrix.shape[0] * image_matrix.shape[1] * 3
+            keys_list = [key for key, _ in sorted(counts.items())][:num_pixels]
+            processed_counts = {
+                key.split(" ")[0] + " " + key.split(" ")[1]: key.split(" ")[2]
+                for key in keys_list
+            }
+        else:
+            num_pixels = image_matrix.shape[0] * image_matrix.shape[1]
+            keys_list = [key for key, _ in sorted(counts.items())][:num_pixels]
+            processed_counts = {
+                key.split(" ")[0]: key.split(" ")[1] for key in keys_list
+            }
 
         return processed_counts
 
@@ -188,6 +228,19 @@ class TestNEQR:
 
         assert np.allclose(result_test_gates, gates_true_list)
 
+    def test_image_rgb_qc_square_matrix_gates(self):
+
+        resized_astronaut_pic = resize(self.ASTRONAUT_IMAGE_RGB, (2, 2))
+        qc = self.NEQR.image_quantum_circuit(
+            image=resized_astronaut_pic, measurements=True
+        )
+
+        circuit_gates = list(qc.count_ops())
+        result_test_gates = [gate in self.GATE_SET for gate in circuit_gates]
+        gates_true_list = np.ones(len(result_test_gates))
+
+        assert np.allclose(result_test_gates, gates_true_list)
+
     def test_image_qc_square_matrix_gate_count(self):
 
         resized_astronaut_pic = resize(self.ASTRONAUT_IMAGE_GRAY, (2, 2))
@@ -224,3 +277,68 @@ class TestNEQR:
         count_gate_list = [hadamard_count, measure_count, x_count, ccx_or_mcx_count]
 
         assert np.allclose(qc_count_gate_list, count_gate_list)
+
+    def test_image_rgb_qc_square_matrix_gate_count(self):
+
+        resized_astronaut_pic = resize(self.ASTRONAUT_IMAGE_RGB, (2, 2))
+        qc = self.NEQR.image_quantum_circuit(
+            image=resized_astronaut_pic, measurements=True
+        )
+
+        qc_gates_dict = dict(qc.count_ops())
+        del qc_gates_dict["barrier"]
+
+        pixel_intensity_dict = self._prepare_pixel_intensity_binary_dict(
+            image_matrix=resized_astronaut_pic
+        )
+
+        hadamard_count = qc.qregs[1].size + qc.qregs[2].size
+        measure_count = qc.cregs[0].size + qc.cregs[1].size + qc.cregs[2].size
+        x_count = np.array(
+            [
+                2 * str.count(key, "0")
+                for key, value in pixel_intensity_dict.items()
+                if value != "0" * 8
+            ]
+        ).sum()
+        ccx_or_mcx_count = np.array(
+            [str.count(bnum, "1") for _, bnum in pixel_intensity_dict.items()]
+        ).sum()
+
+        qc_count_gate_list = [
+            qc_gates_dict["h"],
+            qc_gates_dict["measure"],
+            qc_gates_dict["x"],
+            qc_gates_dict["mcx"],
+        ]
+        count_gate_list = [hadamard_count, measure_count, x_count, ccx_or_mcx_count]
+
+        assert np.allclose(qc_count_gate_list, count_gate_list)
+
+    def test_image_rgb_qc_square_matrix_encoding(self):
+
+        resized_astronaut_pic = resize(self.ASTRONAUT_IMAGE_RGB, (2, 2))
+        qc = self.NEQR.image_quantum_circuit(
+            image=resized_astronaut_pic, measurements=True
+        )
+
+        counts = (
+            execute(experiments=qc, backend=self.BACKEND, shots=self.SHOTS)
+            .result()
+            .get_counts()
+        )
+
+        processed_counts = self._process_counts(
+            counts=counts, image_matrix=resized_astronaut_pic
+        )
+        pixel_intensity_dict = self._prepare_pixel_intensity_binary_dict(
+            image_matrix=resized_astronaut_pic
+        )
+
+        results = [
+            int(value == processed_counts[key])
+            for key, value in pixel_intensity_dict.items()
+        ]
+        pixel_true_list = np.ones(len(results))
+
+        assert np.allclose(results, pixel_true_list)
